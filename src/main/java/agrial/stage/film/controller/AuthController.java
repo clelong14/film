6a6f0c99b83424d6539df115d9d76c4d050e1a3e
Colynne LELONG (AGRIAL) <c.lelong@agrial.com>
2026-01-08 16:54:15 +0100

    Ajout des méthodes sur Swagger pour Cinéma et Authentification

--- src/main/java/agrial/stage/film/controller/AuthController.java
+++ src/main/java/agrial/stage/film/controller/AuthController.java
@@ -4,6 +4,8 @@ import agrial.stage.film.model.Utilisateur;
 import agrial.stage.film.model.dto.UtilisateurDTO;
 import agrial.stage.film.repository.UtilisateurRepository;
 import agrial.stage.film.service.UtilisateurService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +16,14 @@ import java.util.Map;
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin("*")
+@Tag(name = "Authetifications", description = "Gestion des authentifications")
 public class AuthController {
 
     @Autowired
     private UtilisateurService utilisateurService;
 
     @PostMapping("/login")
+    @Operation (summary = "Connexion d'un utilisateur")
     public ResponseEntity<UtilisateurDTO> login(@RequestBody Map<String, String> creds) {
         return utilisateurService.login(creds.get("email"), creds.get("password"))
                 .map(ResponseEntity::ok)
@@ -27,6 +31,7 @@ public class AuthController {
     }
 
     @PostMapping("/register")
+    @Operation (summary = "Inscription d'un utilisateur")
     public UtilisateurDTO register(@RequestBody Utilisateur utilisateur) {
         return utilisateurService.createUtilisateur(utilisateur);
     }
--- src/main/java/agrial/stage/film/controller/CinemaController.java
+++ src/main/java/agrial/stage/film/controller/CinemaController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class CinemaController {
     @Autowired
     private CinemaService cinemaService;
 
+    @GetMapping
     @Operation(summary = "Liste de tous les films")
     public List<Cinema> findAll() {
         return cinemaService.getAllCinemas();
